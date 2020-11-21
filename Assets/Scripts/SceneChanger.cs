0fb01e5d6fb87857adec0cc62363aa2fc5506352
방저녁 <hyeok020514@naver.com>
2020-11-21 20:51:05 +0900

    씬 변경 스크립트(싱글턴) SceneManager

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    private static SceneChanger instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public static SceneChanger Instance
+    {
+        get
+        {
+            if (instance == null)
+                return null;
+            return instance;
+        }
+    }
+
+    public void ChangeScene(string SceneName)
+    {
+        SceneManager.LoadSceneAsync(SceneName);
+    }
+}
