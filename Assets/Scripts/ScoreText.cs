82f63c2c7f3ef2d0f4ab5acdd59085d4f93bb635
방저녁 <hyeok020514@naver.com>
2020-11-21 19:43:11 +0900

    스코어 스크립트 추가 및 구현

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    private Text ScoreText_text;
+    private Animation ScoreText_Animation;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreText_text = GetComponent<Text>();
+        ScoreText_Animation = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetScore(int Score)//추가되는 값이 아니라 걍 Score변수 넣으면 그 값을 똑같이 적용시켜주는거임
+    {
+        ScoreText_text.text = Score.ToString();
+        ScoreText_Animation.Play();
+    }
+}
