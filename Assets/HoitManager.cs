2460a6ad4295c3e3a4bdf2e61578d9917fed25c3
mjun1208 <37032675+mjun1208@users.noreply.github.com>
2020-11-23 23:48:33 +0900

    Revert "길동 맞으면 점수 + 사지분해 관련 함수"
    This reverts commit d7f00c539d75dd1ce81c7262962cb9a373bdc2c0.

---
+++ Assets/HoitManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dooly.Game
+{
+
+    public class HoitManager : MonoBehaviour
+    {
+
+        private const string hoitObjName = "HoitParticle";
+
+        private Queue<GameObject> _hoitQueue = new Queue<GameObject>();
+        private GameObject _hoitGameObject;
+
+        public void Init()
+        {
+            _hoitGameObject = Resources.Load("Prefabs/" + hoitObjName) as GameObject;
+            CreateObject();
+        }
+
+        public void SpawnHoit()
+        {
+        }
+
+        private GameObject CreateObject()
+        {
+            var obj = Instantiate(_hoitGameObject);
+            obj.SetActive(false);
+            obj.transform.SetParent(IngameManager.Instance.Disable.transform);
+
+            _hoitQueue.Enqueue(obj);
+
+            return obj;
+        }
+
+        public void InitObject()
+        {
+            if (_hoitQueue.Count > 0)
+            {
+                var obj = _hoitQueue.Dequeue();
+                obj.transform.SetParent(null);
+                obj.SetActive(true);
+                //return obj;
+            }
+            else
+            {
+                var obj = CreateObject();
+                obj.SetActive(true);
+                obj.transform.SetParent(null);
+                //return newObj;
+            }
+        }
+
+        public void ReleaseObject(GameObject obj)
+        {
+            obj.SetActive(false);
+            obj.transform.SetParent(IngameManager.Instance.Disable.transform);
+            _hoitQueue.Enqueue(obj);
+        }
+    }
+}
