b1cbb84e1e574a048068131ee51490d72d108981
방저녁 <hyeok020514@naver.com>
2020-11-23 23:43:39 +0900

    스코어의 애니메이션을 애니메이터로 변경

--- Assets/Scripts/ScoreText.cs
+++ Assets/Scripts/ScoreText.cs
@@ -6,13 +6,13 @@ using UnityEngine.UI;
 public class ScoreText : MonoBehaviour
 {
     private Text ScoreText_text;
-    private Animation ScoreText_Animation;
+    private Animator ScoreText_Animation;
     
     // Start is called before the first frame update
     void Start()
     {
         ScoreText_text = GetComponent<Text>();
-        ScoreText_Animation = GetComponent<Animation>();
+        ScoreText_Animation = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,6 +24,6 @@ public class ScoreText : MonoBehaviour
     public void GetScore(int Score)//추가되는 값이 아니라 걍 Score변수 넣으면 그 값을 똑같이 적용시켜주는거임
     {
         ScoreText_text.text = Score.ToString();
-        ScoreText_Animation.Play();
+        ScoreText_Animation.Rebind();
     }
 }
