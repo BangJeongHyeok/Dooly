cf7b8b4fc2eb33e19cf87812e328e1b47f9f22a6
방저녁 <hyeok020514@naver.com>
2020-11-23 23:42:35 +0900

    길동 체력 + 힛 + 사지분해 / 길동 관련 함수

---
+++ Assets/Scripts/Gildong.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dooly.Game {
+
+    public class Gildong : MonoBehaviour
+    {
+        public Rigidbody2D Bodyrigid;
+        public GameObject Gildong_obj;
+        List<HingeJoint2D> BodyParts = new List<HingeJoint2D>();
+        int Life = 100;
+
+        private void Start()
+        {
+            GetNewBody(Gildong_obj);
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                IngameManager.HoitManager.InitObject();
+                HitBody(Random.Range(10, 60));//리지드 애드포스
+                IngameManager.Instance.scoretext.GetScore(++IngameManager.Instance.Score);//점수
+                StateChecker();//고길동 체력
+
+            }
+        }
+
+        public void HitBody(float HitPower)
+        {
+            Bodyrigid.AddForce(Vector2.right * HitPower + (Vector2.up * Random.Range(-50f,50f)), ForceMode2D.Impulse);
+        }
+
+        void StateChecker()
+        {
+            Life -= (Random.Range(0, 3)+1);
+            if (Life <= 0)
+            {
+                BodySlash();
+            }
+        }
+
+        public void GetNewBody(GameObject GildongBody)
+        {
+            for (int i = 0; i < GildongBody.transform.GetChild(0).transform.childCount; i++)
+            {
+                HingeJoint2D hinge = GildongBody.transform.GetChild(0).transform.GetChild(i).gameObject.GetComponent<HingeJoint2D>();
+                BodyParts.Add(hinge);
+            }
+        }
+
+        public void BodySlash()
+        {
+            if (BodyParts.Count > 0)
+            {
+                for (int i = 0; i < BodyParts.Count; i++)
+                    BodyParts[i].enabled = false;
+
+                BodyParts.Clear();
+            }
+        }
+    }
+}
