07a77345dbbe87d592f2e74504c6b11862468fc1
김민준 <mjun1208@meerkatgames.co.kr>
2020-11-24 18:45:01 +0900

    스코어 텍스트 애니메 Rebind -> Play로 변경

--- Assets/Scripts/ScoreText.cs
+++ Assets/Scripts/ScoreText.cs
@@ -24,6 +24,6 @@ public class ScoreText : MonoBehaviour
     public void GetScore(int Score)//추가되는 값이 아니라 걍 Score변수 넣으면 그 값을 똑같이 적용시켜주는거임
     {
         ScoreText_text.text = Score.ToString();
-        ScoreText_Animation.Rebind();
+        ScoreText_Animation.Play("ScoreText Animation", -1, 0f);
     }
 }
