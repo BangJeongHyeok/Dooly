5c3ca52782382d21d1014c9b763c76833624f8e1
김민준 <mjun1208@meerkatgames.co.kr>
2020-11-24 18:39:07 +0900

    둘리 매니저 추가 및 구현
    - 호잇이 발사되는 위치를 가지고 있음

---
+++ Assets/Scripts/DoolyManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dooly.Game
+{
+    public class DoolyManager : MonoBehaviour
+    {
+        [SerializeField]private GameObject HoitPivot;
+
+        private void Start()
+        {
+            IngameManager.Instance.SetDoolyManager(this);
+        }
+
+        public Vector2 GetHoitPivot()
+        {
+            return HoitPivot.transform.position;
+        }
+    }
+}
