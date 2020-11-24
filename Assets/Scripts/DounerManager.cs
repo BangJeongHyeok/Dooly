25184f77fa1c4c55809e0a6b570975002f7333a4
김민준 <mjun1208@meerkatgames.co.kr>
2020-11-24 18:46:52 +0900

    도우너 매니저 추가
    - 깐따삐아 쓰는거

---
+++ Assets/Scripts/DounerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dooly.Game
+{
+    public class DounerManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject _dounerPanel;
+        [SerializeField] private Animator _dounerAnim;
+
+        private void Start()
+        {
+            IngameManager.Instance.SetDounerManager(this);
+        }
+
+        public void PlayCosmos()
+        {
+            _dounerPanel.SetActive(true);
+            _dounerAnim.Play("TimeCosmos Animation", -1, 0f);
+        }
+    }
+}
