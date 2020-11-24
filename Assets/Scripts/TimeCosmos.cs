339a1d84c0eaf6daa9d3731888e331ef27373aae
김민준 <mjun1208@meerkatgames.co.kr>
2020-11-24 18:47:52 +0900

    타임 코스모스 스크립트 추가
    - 애니메이션 이벤트에 쓸 함수를 가지고 있음

---
+++ Assets/Scripts/TimeCosmos.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dooly.Game
+{
+    public class TimeCosmos : MonoBehaviour
+    {
+        public void ResurrectionGildong()
+        {
+            IngameManager.Gildong.GetNewBody();
+            this.gameObject.SetActive(false);
+        }
+    }
+}
