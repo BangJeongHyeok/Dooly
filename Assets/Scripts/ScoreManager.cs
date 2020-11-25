54f1b3e4eee9cefb22c9e2d217d359e7886314b3
김민준 <mjun1208@meerkatgames.co.kr>
2020-11-25 15:37:38 +0900

    스코어 매니저 추가 + 이름이 헷갈리는 함수 이름 변경

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Dooly.Game {
+    public class ScoreManager : MonoBehaviour
+    {
+        [SerializeField] private ScoreText scoretext;
+        private int _score = 0;
+
+        public void Start()
+        {
+            IngameManager.Instance.SetScoreManager(this);
+        }
+
+        public void AddScore(int score)
+        {
+            _score += score;
+            scoretext.ApplyScore(_score);
+        }
+    }
+}
