7aef6af39bea0fc89108670b9515ab78b9c52027
김민준 <mjun1208@meerkatgames.co.kr>
2020-11-26 17:21:36 +0900

    랭킹 스크롤 스크립트 추가

---
+++ Assets/Scripts/RankingScroll.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankingScroll : MonoBehaviour
+{
+    [SerializeField] private GameObject _contents;
+    [SerializeField] private GameObject _rankingUnit;
+
+    private void OnGUI()
+    {
+        
+    }
+}
