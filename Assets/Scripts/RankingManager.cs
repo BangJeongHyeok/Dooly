27a783b374449987b35ba73f25e88436d0709689
김민준 <mjun1208@meerkatgames.co.kr>
2020-11-26 17:22:06 +0900

    랭킹매니저 버튼 클릭 함수 추가 (구현 X)

--- Assets/Scripts/RankingManager.cs
+++ Assets/Scripts/RankingManager.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class RankingManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnClick_Ranking()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
