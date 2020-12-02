cc7718f3c13b4e43752f784a313410aa04059f9f
DPeb14 <hyeok020514@gmail.com>
2020-12-03 00:08:54 +0900

    마이콜 매니커(길동으로 진입하는건 안함)

---
+++ Assets/Scripts/MicholManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MicholManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _micholButton;
+    [SerializeField] private Animator _micholAnim;
+
+    bool MicholOn = false;
+
+    void Start()
+    {
+        StartCoroutine(Michol_GetOut());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Michol_GetOut()
+    {
+        yield return new WaitForSeconds(Random.Range(3f, 10f));
+        MicholOn = true;
+        _micholAnim.SetBool("MicholOn", MicholOn);
+        StartCoroutine(Michol_GetOut());
+
+    }
+
+    public void Michol_Getin()
+    {
+        MicholOn = false;
+        _micholAnim.SetBool("MicholOn", MicholOn);
+    }
+}
