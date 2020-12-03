9bdaaee9a9fac86aadff1f1cdfe365e09aa3d590
김민준 <mjun1208@meerkatgames.co.kr>
2020-12-04 00:59:21 +0900

    랭킹 데이터 요청 함수 추가

--- Assets/Scripts/PlayNanooManager.cs
+++ Assets/Scripts/PlayNanooManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using PlayNANOO;
@@ -9,6 +10,7 @@ public class PlayNanooManager : MonoBehaviour
 
     private string _nickName = null;
     private int _score = 0;
+    private ArrayList list = new ArrayList();
 
     public void Init()
     {
@@ -38,19 +40,15 @@ public class PlayNanooManager : MonoBehaviour
         });
     }
 
-    public void ReadRanking()
+    public void RequestRanking(Action<ArrayList> action)
     {
         // 랭킹코드, 몇개 까지 보이는지 (50개가 최대)
         plugin.Ranking("dooly-RANK-BC09F27A-99057B5A", 50, (state, message, rawData, dictionary) =>
         {
             if (state.Equals(Configure.PN_API_STATE_SUCCESS))
             {
-                ArrayList list = (ArrayList)dictionary["list"];
-                foreach (Dictionary<string, object> rank in list)
-                {
-                    Debug.Log(rank["score"]);
-                    Debug.Log(rank["data"]);
-                }
+                list = (ArrayList)dictionary["list"];
+                action.Invoke(list);
             }
             else
             {
