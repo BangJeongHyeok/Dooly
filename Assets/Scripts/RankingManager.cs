56a7ca443ae78c983404d7120ca4ca0ec71075c9
김민준 <mjun1208@meerkatgames.co.kr>
2020-12-04 01:05:36 +0900

    랭킹창 뒤 여백을 클릭시 랭킹창이 꺼지도록 수정

--- Assets/Scripts/RankingManager.cs
+++ Assets/Scripts/RankingManager.cs
@@ -6,10 +6,16 @@ namespace Dooly.Game {
     public class RankingManager : MonoBehaviour
     {
         [SerializeField] private RankingScroll _rankScroll;
+        [SerializeField] private GameObject _margin;
 
         public void OnClick_Ranking()
         {
             IngameManager.PlayNanooManager.RequestRanking(OnResponseRanking);
+        }
+        public void OnClick_Margin()
+        {
+            _rankScroll.gameObject.SetActive(false);
+            _margin.SetActive(false);
         }
 
         private void OnResponseRanking(ArrayList list)
@@ -17,6 +23,7 @@ namespace Dooly.Game {
             IngameManager.PlayNanooManager.WriteRanking();
             _rankScroll.SetRanking(list);
             _rankScroll.gameObject.SetActive(true);
+            _margin.SetActive(true);
         }
     }
 }
