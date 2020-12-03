4113190e3d33524c98c0f65680aec531bc3847e5
김민준 <mjun1208@meerkatgames.co.kr>
2020-12-04 00:56:16 +0900

    랭킹 유닛 스크립트 추가

---
+++ Assets/Scripts/RankingUnit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankingUnit : MonoBehaviour
+{
+    [SerializeField] private Text _rank;
+    [SerializeField] private Text _name;
+    [SerializeField] private Text _score;
+
+    public void SetRankInfo(int rank, string name, int score)
+    {
+        _rank.text = rank.ToString();
+        _name.text = name;
+        _score.text = score.ToString();
+    } 
+}
