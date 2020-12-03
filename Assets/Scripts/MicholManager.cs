936e48b65f6fad8eb300ce74e49c7166099c4ce1
DPeb14 <hyeok020514@gmail.com>
2020-12-04 00:53:26 +0900

    마이콜이 화면의 각 모퉁이에서 나옴

--- Assets/Scripts/MicholManager.cs
+++ Assets/Scripts/MicholManager.cs
@@ -3,35 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MicholManager : MonoBehaviour
-{
-    [SerializeField] private GameObject _micholButton;
-    [SerializeField] private Animator _micholAnim;
-
-    bool MicholOn = false;
-
-    void Start()
+namespace Dooly.Game {
+    public class MicholManager : MonoBehaviour
     {
-        StartCoroutine(Michol_GetOut());
-    }
+        [SerializeField] private GameObject _MicholPivot;
+        [SerializeField] private GameObject _micholButton;
+        [SerializeField] private Animator _micholAnim;
 
-    void Update()
-    {
-        
-    }
+        bool MicholOn = false;
 
-    IEnumerator Michol_GetOut()
-    {
-        yield return new WaitForSeconds(Random.Range(3f, 10f));
-        MicholOn = true;
-        _micholAnim.SetBool("MicholOn", MicholOn);
-        StartCoroutine(Michol_GetOut());
+        void Start()
+        {
+            StartCoroutine(Michol_GetOut());
+        }
 
-    }
+        void Update()
+        {
+        }
 
-    public void Michol_Getin()
-    {
-        MicholOn = false;
-        _micholAnim.SetBool("MicholOn", MicholOn);
+        IEnumerator Michol_GetOut()
+        {
+            yield return new WaitForSeconds(Random.Range(3f, 7f));
+            if (!IngameManager.DounerManager.IsPlayingCosmos)
+            {
+                int RandomPos = Random.Range(0, 4);
+                switch (RandomPos)
+                {
+                    case 0:
+                        _MicholPivot.transform.position = Camera.main.ViewportToScreenPoint(new Vector3(0, 0, 0));
+                        _MicholPivot.transform.localScale = new Vector3(-1, 1, 1);
+                        break;
+                    case 1:
+                        _MicholPivot.transform.position = Camera.main.ViewportToScreenPoint(new Vector3(0, 1, 0));
+                        _MicholPivot.transform.localScale = new Vector3(-1, -1, 1);
+                        break;
+                    case 2:
+                        _MicholPivot.transform.position = Camera.main.ViewportToScreenPoint(new Vector3(1, 0, 0));
+                        _MicholPivot.transform.localScale = new Vector3(1, 1, 1);
+                        break;
+                    case 3:
+                        _MicholPivot.transform.position = Camera.main.ViewportToScreenPoint(new Vector3(1, 1, 0));
+                        _MicholPivot.transform.localScale = new Vector3(1, -1, 1);
+                        break;
+                }
+                MicholOn = true;
+                _micholAnim.SetBool("MicholOn", MicholOn);
+            }
+            StartCoroutine(Michol_GetOut());
+        }
+
+        public void Michol_Getin()
+        {
+            if (MicholOn)
+            {
+                MicholOn = false;
+                _micholAnim.SetBool("MicholOn", MicholOn);
+            }
+        }
     }
 }
