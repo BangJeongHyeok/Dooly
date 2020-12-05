4cf5563fad91011ef493e5ba256e1a21132ef072
김민준 <mjun1208@meerkatgames.co.kr>
2020-12-06 03:31:10 +0900

    마이콜 나타나는 로직 수정

--- Assets/Scripts/MicholManager.cs
+++ Assets/Scripts/MicholManager.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Dooly.Game {
+namespace Dooly.Game
+{
     public class MicholManager : MonoBehaviour
     {
-        [SerializeField] private GameObject _MicholPivot;
         [SerializeField] private GameObject _micholButton;
         [SerializeField] private Animator _micholAnim;
 
@@ -26,26 +26,7 @@ namespace Dooly.Game {
             yield return new WaitForSeconds(Random.Range(3f, 7f));
             if (!IngameManager.DounerManager.IsPlayingCosmos)
             {
-                int RandomPos = Random.Range(0, 4);
-                switch (RandomPos)
-                {
-                    case 0:
-                        _MicholPivot.transform.position = Camera.main.ViewportToScreenPoint(new Vector3(0, 0, 0));
-                        _MicholPivot.transform.localScale = new Vector3(-1, 1, 1);
-                        break;
-                    case 1:
-                        _MicholPivot.transform.position = Camera.main.ViewportToScreenPoint(new Vector3(0, 1, 0));
-                        _MicholPivot.transform.localScale = new Vector3(-1, -1, 1);
-                        break;
-                    case 2:
-                        _MicholPivot.transform.position = Camera.main.ViewportToScreenPoint(new Vector3(1, 0, 0));
-                        _MicholPivot.transform.localScale = new Vector3(1, 1, 1);
-                        break;
-                    case 3:
-                        _MicholPivot.transform.position = Camera.main.ViewportToScreenPoint(new Vector3(1, 1, 0));
-                        _MicholPivot.transform.localScale = new Vector3(1, -1, 1);
-                        break;
-                }
+                _micholButton.SetActive(true);
                 MicholOn = true;
                 _micholAnim.SetBool("MicholOn", MicholOn);
             }
