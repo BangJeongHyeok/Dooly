19611460e2557cd8e72fa6b0fb4bb05d7659ff26
김민준 <mjun1208@meerkatgames.co.kr>
2020-12-06 16:54:16 +0900

    마이콜 칼 던지는 함수 추가, 씬에 적용

--- Assets/Scripts/MicholButton.cs
+++ Assets/Scripts/MicholButton.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 
 public class MicholButton : MonoBehaviour
 {
+    public GameObject IceSword;
+
+    private void OnEnable()
+    {
+        StartCoroutine(HandOverSword());
+    }
+
+    public void Throw_Sword()
+    {
+        IceSword.transform.position = this.transform.position;
+        IceSword.SetActive(true);
+    }
+
+    private IEnumerator HandOverSword()
+    {
+        yield return new WaitForSeconds(Random.Range(2f, 2f));
+
+        if (!IceSword.activeSelf)
+        {
+            Throw_Sword();
+        }
+
+        StartCoroutine(HandOverSword());
+    }
+
     public void GetOutMichol()
     {
         this.gameObject.SetActive(false);
