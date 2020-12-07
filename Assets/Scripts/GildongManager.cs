d8e51e86649968344fe40c1ba208159b10e62401
김민준 <mjun1208@meerkatgames.co.kr>
2020-12-07 11:06:19 +0900

    길동 매니저 추가

---
+++ Assets/Scripts/GildongManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dooly.Game
+{
+    public class GildongManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject _gildongPanel;
+        [SerializeField] private GameObject _gameoverPanel;
+
+        void Start()
+        {
+            IngameManager.Instance.SetGildongManager(this);
+        }
+
+        public void GildongGetSword()
+        {
+            _gildongPanel.SetActive(true);
+        }
+    }
+}
