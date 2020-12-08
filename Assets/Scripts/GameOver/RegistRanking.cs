a9a5ee13b9ea0912c5c35d02eea81c43a81ec4c8
김민준 <mjun1208@meerkatgames.co.kr>
2020-12-08 21:29:38 +0900

    랭킹 입력 스크립트 추가

---
+++ Assets/Scripts/GameOver/RegistRanking.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Dooly {
+    public class RegistRanking : MonoBehaviour
+    {
+        [SerializeField] private Text _nameText;
+        [SerializeField] private Text _scoreText;
+
+        private void Awake()
+        {
+            Global.PlayNanooManager.Init();
+            _scoreText.text = Global.PlayNanooManager.GetScore().ToString();
+        }
+
+        public void WriteRanking()
+        {
+            Global.PlayNanooManager.SetName(_nameText.text);
+            Global.PlayNanooManager.WriteRanking();
+        }
+    }
+}
